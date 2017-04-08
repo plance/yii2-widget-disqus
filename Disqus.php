f250b9848ca9f9333391143dee5c8fecfa667c91
Pavel <pavel.lance@gmail.com>
2017-04-08 16:49:55 +0300

    Autodetection http scheme added

--- Disqus.php
+++ Disqus.php
@@ -14,18 +14,22 @@ class Disqus extends yii\base\Widget
 		{
 			$Request = Yii::$app -> getRequest();
 			
+			$disqus_url = $Request -> hostInfo;
+			$parse_ar = parse_url($disqus_url);
+			
 			if($this -> disqus_url === null)
 			{
-				$disqus_url = $Request -> getHostInfo().$Request -> getUrl();
+				$disqus_url .= $Request -> url;
 			}
 			else
 			{
-				$disqus_url = $Request -> getHostInfo().$this -> disqus_url;
+				$disqus_url .= $this -> disqus_url;
 			}
 			
 			return $this -> render('disqus',[
 				'disqus_shortname' => $this -> disqus_shortname,
 				'disqus_url' => $disqus_url,
+				'scheme' => $parse_ar['scheme']
 			]);
 		}
 		return '';
